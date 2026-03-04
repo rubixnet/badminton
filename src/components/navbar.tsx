3ef47e161d3578975383fd97ab96a7dbfc897f70
rubix <gauravbhaturkar18@gmail.com>
2026-03-04 15:46:53 +0530

    updated and made navbar simple

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,21 @@
 
 const Navbar = () => {
     return (
-        <div>
-            Badminton Tracker       </div>
-        );
+        <div className="bg-background">
+            <div className="container max-w-6xl mx-auto px-6">
+                <div className="flex items-stretch justify-between">
+                    <div className="flex items-center px-2 py-4">
+                        <h1 className="text-xl font-bold">Badminton Tracker</h1>
+                    </div>
+                    <div className="flex items-stretch">
+                        <button className="h-full px-6 bg-background text-primary hover:bg-accent hover:text-accent-foreground border-none">
+                            Create Match
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
