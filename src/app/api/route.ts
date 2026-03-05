020c5c31ca499f784df99da17b05ae00c044c574
rubix <gauravbhaturkar18@gmail.com>
2026-03-05 12:01:37 +0530

    added get / post to send and get data from google sheets

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getGoogleSheetsClient } from '@/lib/google-sheets';
 import type { Match } from '@/types/match';
+import { start } from 'repl';
 
 const SHEET_ID = process.env.GOOGLE_SHEET_ID;
 const SHEET_NAME = "Sheet1";
@@ -10,10 +11,98 @@ export async function GET(request: NextRequest) {
         if (!SHEET_ID) {
             console.error("missing google sheet id");
             return NextResponse.json({ error: "Google Sheet ID is not configured" }, { status: 500 });
-    )
+        }
 
     const { searchParams } = new URL(request.url);
     const page = parseInt(searchParams.get('page') || '1');
-    const perPage = parseInt(searchParams.get('limit') || '20');
+    const limit = parseInt(searchParams.get('limit') || '20');
 
     const sheets = await getGoogleSheetsClient();
+
+
+    const countResponse = await sheets.spreadsheets.values.get({
+        spreadsheetId: SHEET_ID,
+        range: `${SHEET_NAME}!A:A`
+    });
+    
+    const totalRows = countResponse.data.values ? countResponse.data.values.length - 1 : 0; // Subtract header row
+    
+    if (totalRows <= 1) {
+        return NextResponse.json({ matches: [], total: 0, page, totalPages: 0 });
+    }
+
+
+    const dataRows = totalRows - 1
+    const totalPages = Math.ceil(dataRows / limit); // to round up as this be something like x / 20 
+
+
+    const endIndex = totalRows - (page - 1) * limit
+    const startIndex = endIndex - limit + 1
+
+    // maths for above in google sheets
+    // Data at row 2. 1 is for name of the file
+    // Total data rows = totalRows - 1 ()
+    // Latest match is at row `totalRows`
+    // Page 1 (limit 20): Rows (totalRows - 19) to totalRows
+
+    if (startIndex < 2) startIndex = 2 // not be low header row
+
+    if (endIndex < 2) {
+        return NextResponse.json({ matches: [], total: totalRows - 1, page, totalPages });
+    }
+
+    const range = `${SHEET_NAME}!A${startIndex}:E${endIndex}`;
+
+    const response = await sheets.spreadsheets.values.get({
+        spreadsheetId: SHEET_ID,
+        range,
+    })
+
+    const rows = response.data.values || [];
+
+    const matches: Match[] = rows.map((row: string[]) => ({
+        id: row[0],
+        createdAt: row[1], // to add this in submit form
+        team1 : {
+            score: parseInt(row[3]) || 0,
+            players: [
+                { team1player1: row[5], team1player2: row[7] },
+            ]
+        team2 : {
+            score: parseInt(row[4]) || 0,
+            players: [
+                { team2player1: row[9], team2player2: row[11] },
+            ]
+        
+        }
+
+    }))
+
+    return NextResponse.json({ matches, total: dataRows, page, totalPages });
+    }
+
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const sheets = await getGoogleSheetsClient();
+
+    const match = {
+        id: `match-${Date.now()}`,
+        createdAt: body.createdAt,
+        team1 : body.team1,
+        team2 : body.team2,
+        winner: { "1": "team1", "-1": "team2" }[Math.sign(body.team1.score - body.team2.score)]
+    }; 
+
+    const values = [[match.id, match.createdAt, match.team1.score, match.team1.players[0].team1player1, match.team1.players[0].team1player2, match.team2.score, match.team2.players[0].team2player1, match.team2.players[0].team2player2]];
+
+    await sheets.spreadsheets.values.append({
+        spreadsheetId: SHEET_ID,
+        range: `${SHEET_NAME}!A:N`,
+        valueInputOption: "RAW",
+        requestBody: { values}
+    });
+
+    return NextResponse.json({ match });
+}
+
